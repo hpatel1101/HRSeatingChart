3f117c9cd18904f4b278027a1f3e82087c062336
hardik <160777787+hpatel1101@users.noreply.github.com>
2026-09-07 23:15:31 -0400

    Add seating data from final Excel chart

---
+++ seating-data.js
@@ -0,0 +1,300 @@
+const SEATING_DATA = [
+  {
+    "table": "Family Table 1",
+    "guests": [
+      "Sushila Patel",
+      "Aruna Patel",
+      "Manish Patel",
+      "Rimisha Patel",
+      "Hitesh Patel",
+      "Mita Patel",
+      "Eshan Patel",
+      "Dylan Patel",
+      "Shayan Patel",
+      "Paresha Patel",
+      "Hari Patel",
+      "Nilkanth Patel",
+      "Mahima Patel",
+      "Jatin Patel",
+      "Bharti Patel",
+      "Bhavin Patel",
+      "Deepak Patel",
+      "Darshana Patel",
+      "Jyoti Patel",
+      "Mitool Patel",
+      "Sasha Mohabir",
+      "Shannon Mohabir",
+      "Sohil Patel",
+      "Karishma Patel"
+    ]
+  },
+  {
+    "table": "Family Table 2",
+    "guests": [
+      "Manish Patel",
+      "Hetal Patel",
+      "Walter Grayson",
+      "Barbara Grayson",
+      "Vedika Patel",
+      "Priyesh Patel",
+      "Kiran Patel",
+      "Dilip Patel",
+      "Divya Patel",
+      "Manjula Patel",
+      "Dharmesh Patel",
+      "Krupali Patel",
+      "Savan Patel",
+      "Riya Patel",
+      "Pujan Patel",
+      "Jignesh Patel",
+      "Bhavini Patel",
+      "Kishan Patel",
+      "Aenika Patel",
+      "Ketan D Patel",
+      "Tina K Patel",
+      "Dixita D Patel",
+      "Ishani K Patel",
+      "Karina K Patel"
+    ]
+  },
+  {
+    "table": "1",
+    "guests": ["Shantilal Patel","Bharti Patel","Dhirajlal Patel","Subhadra Patel","Mukund Patel","Sunila Patel","Manisha Patel","Hetal Patel","Nimish Patel","Nimisha Patel"]
+  },
+  {
+    "table": "2",
+    "guests": ["Narendra Patel","Jaya Patel","Champak Patel","Shaku Patel","Usha Patel","Mafatlal Patel","Meena Patel","Ambulal Patel","Kapila Patel"]
+  },
+  {
+    "table": "3",
+    "guests": ["Kiran Patel","Kokila Patel","Harish Patel","Jayshree Patel","Dipika Engineer","Mehul Engineer","Harshika Patel","Dinesh Patel","Nisha Patel","Nimesh Patel"]
+  },
+  {
+    "table": "4",
+    "guests": ["Tilak Patel","Harni Patel","Soham Thakker","Avani Patel","Sachet Amin","Deep Patel","Aayush Patel","Veer Patel","Daylen Talekar","Mit Patel"]
+  },
+  {
+    "table": "5",
+    "guests": ["Nand Patel","Iesh Patel","Tilak G. Patel","Aashay Patel","Rishbh Patel","Jainil Patel","Jay Slushy Patel","Jay Piyush Patel","Kirtan Patel","Krish Patel","Yog Patel"]
+  },
+  {
+    "table": "6",
+    "guests": ["Alex Henry","Janae","Ben Kelly","Isabel Kelly","Shivam Patel","Sejal Gandhi","Riti Yalamanchili","Said Al-Jazaeri","Mehul Patel","Kirtan Bhatt"]
+  },
+  {
+    "table": "7",
+    "guests": ["Jack Nawrocki","Elizabeth Murphy","Sydney Mosteller","Seungmeen Rhee","Cierra Mckenzie","Zac Meyers","Maddie Meyers","Tyce Lindhout","Vicky Kuprewicz"]
+  },
+  {
+    "table": "8",
+    "guests": ["Ketan Patel","Keshina Patel","Nimesh Patel","Monal Patel","Jigna Patel","Piyush Patel","Nirav Kotia","Sandeep Patel","Jalpa Patel"]
+  },
+  {
+    "table": "9",
+    "guests": ["Neera Patel","Neal Patel","Ria Patel","Kush Patel","Prit Patel","Hursh Patel","Riya Patel","Raj Patel","Helly Patel","Raina Patel"]
+  },
+  {
+    "table": "10",
+    "guests": ["Akshar Patel","Tej Patel","Markand Patel","Jay Patel","Riya Patel","Shweta Patel","Diya Patel","Jayant Patel","Shlok Patel","Aaryan Patel"]
+  },
+  {
+    "table": "11",
+    "guests": ["Priyam Patel","Khyati Patel","Bhakti Patel","Mehul Patel","Priyanka Patel","Yogin Patel","Bharat Patel","Bela Patel","Hitesh Patel","Trupati Patel"]
+  },
+  {
+    "table": "12",
+    "guests": ["Ruchi Iyengar","Sachin Iyengar","Sanjay Patel","Shital Patel","Vipin Parmar","Vimpi Parmar","Bhavana Amin","Pinesh Amin","Ashini Parikh"]
+  },
+  {
+    "table": "13",
+    "guests": ["Aekta Patel","Vikas Patel","Jaanvi Patel","Radhi Brahmbhatt","Mit Brahmbhatt","Akshara Brahmbhatt","Raksha Brahmbhatt","Krishna Patel","Ravi Patel","Meera Patel","Arjun Patel","Trusha Patel"]
+  },
+  {
+    "table": "14",
+    "guests": ["Drasti Patel","Shilam Patel","Ami Patel","Dhir Patel","Anisha Patel","Raj Patel","Priyum Patel","Krishna Patel","Bhavik Patel","Alora Patel","Kaiyan Patel"]
+  },
+  {
+    "table": "15",
+    "guests": ["Taram Engineer","Raag Engineer","Janaki Engineer","Ami Patel","Kaveera Patel","Kai Patel","Jaiden Patel","Ish Contractor","Mona Contractor","Prem Contractor","Binal Patel"]
+  },
+  {
+    "table": "16",
+    "guests": ["Jatin Patel","Moneeka Patel","Jitesh Patel","Purvi Patel","Pratima Patel","Sangeeta Patel","Lalit Patel","Prexsha Patel","Tanuja Patel"]
+  },
+  {
+    "table": "17",
+    "guests": ["Riddhi Patel","Kishan Patel","Kush Patel","Mansi Patel","Kirtan Patel","Janki Patel","Vandan (Dino) Patel","Jay Kishan Patel","Raj Patel","Radhika Patel","Maharshi Patel"]
+  },
+  {
+    "table": "18",
+    "guests": ["Arlene Mohabir","Naresh Mohabir","Bhavini Patel","Anil Patel","Dinali Patel","Samir Patel","Sonal Patel","Simmi Patel","Mrudula Patel"]
+  },
+  {
+    "table": "19",
+    "guests": ["Shivam Patel","Nisha Patel","Jay Patel","Sarika Patel","Mila Patel","Akshar Patel","Varsha Patel","Pradip Patel","Hitesh Patel","Smita Patel"]
+  },
+  {
+    "table": "20",
+    "guests": ["Hemal Patel","Hetal Patel","Alpa Patel","Daxesh Patel","Pinkesh Patel","Nirmita Patel","Parul Patel","Janit Patel","Parul (Paresh) Patel"]
+  },
+  {
+    "table": "21",
+    "guests": ["Rakesh Patel","Dharmesh Patel","Kamlesh Patel","Bina Patel","Arjun Patel","Murti Patel","Mukta Patel"]
+  },
+  {
+    "table": "22",
+    "guests": ["Pinal Patel","Kirti Patel","Piyush Patel","Minal Patel","Viraj Patel","Nidhi Patel","Kinnary Patel","Kaushal Patel","Bhupendra Patel","Mona Patel"]
+  },
+  {
+    "table": "23",
+    "guests": ["Manan Gandhi","Sejal Gandhi","Ketan Shah","Gauri Shah","Vaibhav Shah","Suchi Shah","Ruhee Gandhi","Misha Gandhi","Prachi Patel","Jatan Patel"]
+  },
+  {
+    "table": "24",
+    "guests": ["Bipin Patel","Nimisha Patel","Ajay Patel","Reshma Patel","Yatin Patel","Deval Patel","Kirit Patel","Ranjan Patel","Harshad Patel","Illakshi Patel"]
+  },
+  {
+    "table": "25",
+    "guests": ["Arvind Patel","Nayana Patel","Jaymin Patel","Mitali Patel","Hasit Amin","Tejal Amin","Ashok Patel","Ramila Patel","Jitendra Patel","Parul Patel"]
+  },
+  {
+    "table": "26",
+    "guests": ["Paras Patel","Sushma Patel","Hemu Patel","Kishan Patel","Unnati Patel","Parul Patel","Dinesh Patel","Nimixa Patel","Hiten Patel","Sunita Patel"]
+  },
+  {
+    "table": "27",
+    "guests": ["Jaymin Patel","Kinjal Patel","Sagar Patel","Prachi Patel","Akshay Patel","Dhara Patel","Tushar Patel","Jalpa Patel","Premy Patel","Andy Patel"]
+  },
+  {
+    "table": "28",
+    "guests": ["Kalpesh Patel","Rupal Patel","Dhaval Patel","Parita Patel","Chirag Patel","Mitali Patel","Vijay Patel","Nilesha Patel","Prekshu Patel","Bipin Patel"]
+  },
+  {
+    "table": "29",
+    "guests": ["Anil Patel","Vanita Patel","Rishika Patel","Mitesh Patel","Hiral Patel","Lila Patel","Aruna Patel","Meena Patel"]
+  },
+  {
+    "table": "30",
+    "guests": ["ManishKumar Patel","Krupali M Patel","Chirag M Desai","Monica C Desai","Uday P Patel","Toral U Patel","Jal U Patel","Kajal Desai","Jignesh Desai"]
+  },
+  {
+    "table": "31",
+    "guests": ["Harsh Patel","Shreeja Shah(Patel)","Parth Patel","Priya Patel","Mehul Patel","Nimisha Patel","Divya R Patel","Kailashba Patel","Vani Patel","Swar Patel"]
+  },
+  {
+    "table": "32",
+    "guests": ["Viraj Patel","Visha Patel","Parth Patel","Palak Patel","Rushang Patel","Bansari Patel","Dhruti Patel","Ishant Patel","Kaival Patel","Dhruv Patel"]
+  },
+  {
+    "table": "33",
+    "guests": ["Naiya Patel","Ritu Patel","Rahul Patel","Chandni Patel","Vaidehi Patel","Hardik Patel","Jina Patel","Bhumi Patel","Deepa Patel","Kamal Patel"]
+  },
+  {
+    "table": "34",
+    "guests": ["Tanvi Gujral","Sara Mohammad","Avni Mahatme","Sarika Mahatme","Mayuresh Mahatme","Sanjh Mahatme","Shree Muralidaran","Dev Yadav","Priya Kumar","Disha Adama"]
+  },
+  {
+    "table": "35",
+    "guests": ["Khushi Amin","Sakshi Patel","Dhiti Patel","Diya Patel","Keshav Patel","Riya Patel","Janki Patel","Vinit Patel","Jay Vijay Patel","Sujal Patel"]
+  },
+  {
+    "table": "36",
+    "guests": ["Daniel Hannapel","Aiden Hannapel","Emma Decker","Dane Smitz","Alexis Smitz","Kirti J Patel","Marilyn Clow","David Clow","Brendan Elliot"]
+  },
+  {
+    "table": "37",
+    "guests": ["Lisa Patel","Dhanvi Kantamneni","Maddie Phillips","Jeri Grundhoefer","Daniel Moreno","Emma Price","Aaron Norfleet","Emily Belford","Alexa Samani","Andrew Zipp"]
+  },
+  {
+    "table": "38",
+    "guests": ["Indumasi Patel","Mamta P Patel","Binoy P Patel","Khushboo Khatrani(patel)","Varsha U Patel","Uday Patel","Jagruti Patel","Deepak Patel","Manisha D Patel"]
+  },
+  {
+    "table": "39",
+    "guests": ["Manjufoi Patel","Rita N Patel","Mayank Patel","Manisha M Patel","Satish Patel","Bhavna Patel","Purnima Patel","Dinesh Patel","Jagruti Patel"]
+  },
+  {
+    "table": "40",
+    "guests": ["Bhavna Patel","Bharat Patel","Dharini Patel","Nipun Patel","Praful Patel","Sheetal Patel","Leela Patel","Ritu Patel","Ketan Patel"]
+  },
+  {
+    "table": "41",
+    "guests": ["Shivani Patel","Mihir Patel","Sahajanand Patel","Keya Patel","Nishtha Patel","Sheetal Gajjar","Jaydeep Gajjar","Nikhil Gajjar","Bhavin Gajjar"]
+  },
+  {
+    "table": "42",
+    "guests": ["Vishek Patel","Khushi Patel","Isha Patel","Nand Patel","Chandni Patel","Nitya Patel","Neya Patel","Palak Patel","Riya Patel","Neha Patel"]
+  },
+  {
+    "table": "43",
+    "guests": ["Rupal Patel","Deepak Patel","Kamini Patel","Drishti Patel","Nilam Patel","Shashi Patel","Dhara Pandya","Ashish Pandya","Nihit Patel","Kiran Patel"]
+  },
+  {
+    "table": "44",
+    "guests": ["Urvashi Pandya","Krupali Pandya","Sharad Pandya","Shivangi Patel","Kishore Pandya","Mansukh Bhanderi","Ranjan Bhanderi","Rita Patel","Jitu Patel"]
+  },
+  {
+    "table": "45",
+    "guests": ["Dharmesh Patel","Jayshri Patel","Priti Patel","Kamleshkumar Patel","Bipin Patel","Heena Patel","Sunil Patel","Dhara Patel","Atul Patel","Priti Patel"]
+  },
+  {
+    "table": "46",
+    "guests": ["Aan Patel","Nandni Patel","Hirni Patel","Maahi Patel","Anvi Patel","Anya Patel","Vani Patel","Aneri Patel","Anusha Patel","Aarna Patel"]
+  },
+  {
+    "table": "47",
+    "guests": ["Aaryan Patel","Vihan Iyengar","Nischay Patel","Tilak Kakadia","Ved Patel","Shrey Patel","Shivani Patel","Moksha Parmar","Krisha Patel","Nishta Kakadia"]
+  },
+  {
+    "table": "48",
+    "guests": ["Komal Sahu","Kunal Sahu","Vira Sahu","Kamal Satuluru","Namrata Satuluru","Supraja Satuluru","Madhvi Kasam","Nagesh Kasam","Sivananda Nyayapathi"]
+  },
+  {
+    "table": "49",
+    "guests": ["Mitesh Patel","Dimple Patel","Jigar Patel","Dipali Patel","Ritesh Desai","Sheetal Desai","Chintu Dave","Khyati Dave","Kashyap Patel","Dhvani Patel"]
+  },
+  {
+    "table": "50",
+    "guests": ["Mayank Patel","Chandani Patel","Niyam Patel","Neela Patel","JB Patel","Chandrika Patel","Ghanshyam Patel","Kirna Parikh","Bela Patel"]
+  },
+  {
+    "table": "51",
+    "guests": ["Rajesh Patel","Vaishali Patel","Kamlesh Patel","Kalpana Patel","Nita Patel","Suresh Patel","Avani Patel","Sonal Patel","Hiral Patel","Jayna Thakkar"]
+  },
+  {
+    "table": "52",
+    "guests": ["Tejal Patel","Ketan Patel","Pratima Patel","Nalisha Patel","Minal(sumi) Patel","Hitesh Patel","Leela Patel","Falguni Patel","Ripal Patel"]
+  },
+  {
+    "table": "53",
+    "guests": ["Sundip Lamba","Srinivas Nathani","Vikranth Gottipatti","Sreenivas Chintamaneni","Utsav Patel","Pranay Patel","Jalpa Patel","Shivay Patel","Sandip Patel","Kamini Patel"]
+  },
+  {
+    "table": "54",
+    "guests": ["Lata Patel","Bipin Patel","Rajni Patel","Tarlika Patel","Mukesh Chandan","Dipti Chandan","Atush Patel","Prerna Patel","Kalpesh(dmj) Patel","Ritu Patel"]
+  },
+  {
+    "table": "55",
+    "guests": ["Rageshri Metha","Rupande Metha","Anand Metha","Jayshree Patel","Rajni Patel","Anil Patel","Dipti Patel","Amrut Patel","Vanita Patel"]
+  },
+  {
+    "table": "56",
+    "guests": ["Nipa Barot","Vimal Barot","Krishna Patel","Naishad Patel","Hiren Patel","Nirali Patel","Bhavita Patel","Piyush Patel","Minal Patel","Amrish Patel"]
+  },
+  {
+    "table": "57",
+    "guests": ["Pravin Patel","Sudhaben Patel","Harshad Patel","Meena Patel","Bhavini Desai","Janu Desai","Gautam Patel","Saroj Patel","Drupal Patel","Dipen Patel"]
+  },
+  {
+    "table": "58",
+    "guests": ["Mahesh Patel","Jaya Patel","Urmila Patel","Saroj Patel","Kusum Patel","Jash Patel","Prafula Amin","Ramesh Amin","Gordhan Patel","Joshana Patel"]
+  },
+  {
+    "table": "59",
+    "guests": ["Panchshil Patel","Hemangini Patel","Kalpesh Patel","Kamini Patel","Deepali Tukaya","Kiran Tukaya","Anisha Desai","Jignesh Desai","Mahesh Patel","Sheetal Patel"]
+  },
+  {
+    "table": "60",
+    "guests": ["Avani Patel","Pritesh Patel","Prapti Patel","Ashish Kakadia","Swati Kakadia","Vishal Patel","Sarina Patel","Sweta Patel"]
+  }
+];
